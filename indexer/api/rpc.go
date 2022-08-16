ceef99768d12bae7987279ffa475f3b0e12abee0
Giancarlos Salas <me@giansalex.dev>
2022-08-15 21:30:31 -0500

    Add eventbus methods

--- indexer/api/rpc.go
+++ indexer/api/rpc.go
@@ -5,15 +5,29 @@ import (
 	"net"
 	"net/http"
 
+	tmconfig "github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/libs/log"
 	tmpubsub "github.com/tendermint/tendermint/libs/pubsub"
+	rpccore "github.com/tendermint/tendermint/rpc/core"
 	rpcserver "github.com/tendermint/tendermint/rpc/jsonrpc/server"
 	"github.com/tendermint/tendermint/state/txindex"
 	ttypes "github.com/tendermint/tendermint/types"
 )
 
 func StartRPC(listener string, indexer txindex.TxIndexer, eventBus *ttypes.EventBus, logger log.Logger) ([]net.Listener, error) {
+	rpccore.SetEnvironment(&rpccore.Environment{
+		EventBus: eventBus,
+		Logger:   logger.With("module", "rpc"),
+
+		Config: *tmconfig.DefaultRPCConfig(),
+	})
+
 	var routes = map[string]*rpcserver.RPCFunc{
+		// subscribe/unsubscribe are reserved for websocket events.
+		"subscribe":       rpcserver.NewWSRPCFunc(rpccore.Subscribe, "query"),
+		"unsubscribe":     rpcserver.NewWSRPCFunc(rpccore.Unsubscribe, "query"),
+		"unsubscribe_all": rpcserver.NewWSRPCFunc(rpccore.UnsubscribeAll, ""),
+
 		"tx":        rpcserver.NewRPCFunc(NewTx(indexer), "hash"),
 		"tx_search": rpcserver.NewRPCFunc(NewTxSearch(indexer), "query,page,per_page,order_by"),
 	}
